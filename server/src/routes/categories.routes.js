989318189256f65ccf3807213468ca18a25825d9
Marcela3-DesFullStack <claudiamarcela.nc14@gmail.com>
2024-04-05 21:52:50 +0200

    Feat: CRUD routes into posts

--- server/src/routes/categories.routes.js
+++ server/src/routes/categories.routes.js
@@ -5,8 +5,8 @@ import { prisma } from "../database/dbprisma.js"
 const router = Router();
 
 router.get('/categories', async (req, res) => {
-    const categoriesAll = await prisma.categories.findMany()
-    res.json(categoriesAll)
+    const categories = await prisma.categories.findMany()
+    res.json(categories)
 })
 
 export default router
