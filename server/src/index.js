f015d4b32068ddb0007eadd89e83455f58d4efad
Marcela3-DesFullStack <claudiamarcela.nc14@gmail.com>
2024-04-11 09:58:36 +0200

    Feat: App Routes login and front login component

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ import categoriesRoutes from './routes/categoriesRoutes.js'
 import usersRoutes from './routes/usersRoutes.js'
 import commentsRoutes from './routes/commentsRoutes.js'
 import imagesRoutes from './routes/imagesRoutes.js'
-
+// import loginRoutes from './routes/loginRoutes.js'
 
 const app = express(); // ejecutar express
 
@@ -16,6 +16,11 @@ app.use ("/categories/", categoriesRoutes);
 app.use ("/users/", usersRoutes);
 app.use ("/comments", commentsRoutes);
 app.use ("/images", imagesRoutes);
+
+// Login y autenticación
+// app.get("/login", loginRoutes);
+
+
 
 app.listen(3000)
 console.log('Server on port', 3000);
